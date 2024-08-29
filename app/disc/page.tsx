d65d018b85efb67dec0bc448ed6ef8701ef09681
duong thai <thaiq9577@gmail.com>
2024-08-29 14:58:41 +0700

    init code

---
+++ app/disc/page.tsx
@@ -0,0 +1,11 @@
+import Test from "./_component/Test";
+
+const DISC = () => {
+  return (
+    <div className="mt-6">
+      <Test />
+    </div>
+  );
+};
+
+export default DISC;
---
+++ components/footer.tsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return (
+        <div className="">
+            <div className="max-w-7xl py-4 m-auto border-t-2 border-gray-400">
+                <div className="flex justify-between">
+                    <p className="text-xs text-gray-700">
+                        Bản quyền © 2024 Apple Inc. Bảo lưu mọi quyền.
+                    </p>
+                    <p className="text-xs text-gray-700">
+                        Việt Nam
+                    </p>
+                </div>
+                <div className="text-xs text-gray-700">
+                    Chính Sách Quyền riêng Tư | Điều Khoản Sử Dụng | Bán Hàng và Hoàn Tiền | Pháp Lý | Sơ Đồ Trang Web
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
